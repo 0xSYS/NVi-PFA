850f3c714df9050a6c01a1faddd340e5f18fa1ca
0xsys <andreicu07@gmail.com>
2025-04-27 21:59:27 +0300

    removed internal cached stuff

--- app/build/intermediates/cxx/Debug/5m5g4s6c/logs/arm64-v8a/build_command_main.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Hexagon\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Downloads\\nvi pfa\\NVi-PFA\\app\\.cxx\\Debug\\5m5g4s6c\\arm64-v8a" ^
-  main
--- app/jni/src/build/CMakeFiles/nvi-pfa.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for nvi-pfa.
-# This may be replaced when dependencies are built.
