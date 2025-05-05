2942a53ce1ae610b0b5de23196038eadfc174c28
0xsys <andreicu07@gmail.com>
2025-05-06 00:01:35 +0300

    No gui demo

--- app/jni/src/Gui.cxx
+++ app/jni/src/Gui.cxx
@@ -310,10 +310,10 @@ void NVGui::Run(SDL_Renderer *r)
     const float longClickThreshold = 0.5f;
     
     // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-    if (show_demo_window)
-    {
-        ImGui::ShowDemoWindow(&show_demo_window);
-    }
+    //if (show_demo_window)
+    //{
+    //    ImGui::ShowDemoWindow(&show_demo_window);
+    //}
     
     // Reserved for future use
     //ImGui::Text("%.1f FPS", nvg.io.Framerate);
